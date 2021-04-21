529e3fff3e764f42ff63fea9f5e123e8691fc084
Projectcoded <projectcodedorg@gmail.com>
2021-04-21 09:35:11 +0530

    oo

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class _TodoAppState extends State<TodoApp> {
         onPressed: () {
           print("Add it");
         },
-        child: Icon(CarbonIcons.music),
+        child: Icon(CarbonIcons.add),
       ),
     );
   }
