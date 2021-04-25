506ac704a301a484f1d2fe8f089260520f9d03ca
madd-project <projectcodedorg@gmail.com>
2021-04-25 21:37:03 +0530

    dat 4 (same changes)

---
+++ lib/models/todo_model.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todo_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TodoModelAdapter extends TypeAdapter<TodoModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  TodoModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return TodoModel(
+      todoName: fields[0] as String,
+      todoDescription: fields[1] as String,
+      isCompleted: fields[4] as bool,
+      todoRemainder: fields[3] as DateTime,
+      todoEmoji: fields[2] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, TodoModel obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.todoName)
+      ..writeByte(1)
+      ..write(obj.todoDescription)
+      ..writeByte(2)
+      ..write(obj.todoEmoji)
+      ..writeByte(3)
+      ..write(obj.todoRemainder)
+      ..writeByte(4)
+      ..write(obj.isCompleted);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TodoModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
